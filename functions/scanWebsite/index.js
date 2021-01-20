bfcccb1ad2f6342f856d87db919fbae774674dc3
shopint2021 <shopint2021@gmail.com>
2021-01-20 21:23:44 +0200

    reformat

--- functions/scanWebsite/index.js
+++ functions/scanWebsite/index.js
@@ -1,102 +1,105 @@
 const queue = require('queue');
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 
 const { STAGE, InvokePort } = process.env;
 console.log(STAGE);
-const Lambda = STAGE === 'prod' ? new AWS.Lambda() : new AWS.Lambda({
-  endpoint: `http://host.docker.internal:${InvokePort}`,
-  sslEnabled: false
-});
+const Lambda =
+    STAGE === 'prod'
+        ? new AWS.Lambda()
+        : new AWS.Lambda({
+              endpoint: `http://host.docker.internal:${InvokePort}`,
+              sslEnabled: false,
+          });
 
 async function invokeLambda({ functionName, payload }) {
-  const req = {
-    FunctionName: functionName,
-    Payload: JSON.stringify(payload),
-    InvocationType: "RequestResponse",
-  };
-  // console.log(JSON.stringify(payload));
-  const response = await Lambda.invoke(req).promise();
-  // console.log(JSON.stringify(response));
-  const { Payload } = response;
-  return JSON.parse(Payload);
+    const req = {
+        FunctionName: functionName,
+        Payload: JSON.stringify(payload),
+        InvocationType: 'RequestResponse',
+    };
+    // console.log(JSON.stringify(payload));
+    const response = await Lambda.invoke(req).promise();
+    // console.log(JSON.stringify(response));
+    const { Payload } = response;
+    return JSON.parse(Payload);
 }
 
 exports.handler = async (event) => {
-  const { url } = event;
+    const { url } = event;
 
-  const crawlQueue = queue({ results: [] });
-  const searchQueue = queue({ results: [] });
-  const insertQueue = (queue, href) => {
-    queue.push(async (cb) => {
-      const result = await scanUrl(href);
-      // console.log(result);
-      cb(null, result);
-    });
-  }
-  
-  const scanUrl = async url => {
-    let { hrefs, product } = await invokeLambda({ functionName: 'getWebsiteData', payload: { url } });
-    if (product) {
-      product.url = url;
-      products.push(product);
-      console.log(JSON.stringify(product));
-    }
-    if (hrefs) {
-      console.log(`hrefs: ${hrefs.length}`);
-      for (const href of hrefs) {
-        if (!visitedURLs.includes(href)) {
-          visitedURLs.push(href);
-          insertQueue(crawlQueue, href);
-          insertQueue(searchQueue, href);
+    const crawlQueue = queue({ results: [] });
+    const searchQueue = queue({ results: [] });
+    const insertQueue = (queue, href) => {
+        queue.push(async (cb) => {
+            const result = await scanUrl(href);
+            // console.log(result);
+            cb(null, result);
+        });
+    };
+
+    const scanUrl = async (url) => {
+        let { hrefs, product } = await invokeLambda({ functionName: 'getWebsiteData', payload: { url } });
+        if (product) {
+            product.url = url;
+            products.push(product);
+            console.log(JSON.stringify(product));
         }
-      }
-    } else {
-      // console.log('no hrefs');
-    }
-    return url;
-  }
-
-  async function searchProduct(product) {
-    let { searchResults } = await invokeLambda({ functionName: 'getSearchResults', payload: { product } });
-    // if (hrefs) {
-    //   console.log(hrefs.length);
-    //   console.log(product);
-    //   for (const href of hrefs) {
-    //     if (!visitedURLs.includes(href)) {
-    //       visitedURLs.push(href);
-    //       insertQueue(href);
-    //     }
-    //   }
-    // } else {
-    //   // console.log('no hrefs');
-    // }
-    // return url;
-  }
-  
-  const wait = () => {
-    return new Promise((resolve, reject) => {
-      crawlQueue.start((err) => {
-        if (err) console.log(err);
-        else {
-          console.log(`visitedURLs: ${visitedURLs.length}`);
-          console.log(`products: ${products.length}`);
-          resolve({
-            visitedURLs,
-            products,
-          });
+        if (hrefs) {
+            console.log(`hrefs: ${hrefs.length}`);
+            for (const href of hrefs) {
+                if (!visitedURLs.includes(href)) {
+                    visitedURLs.push(href);
+                    insertQueue(crawlQueue, href);
+                    insertQueue(searchQueue, href);
+                }
+            }
+        } else {
+            // console.log('no hrefs');
         }
-      });
+        return url;
+    };
+
+    async function searchProduct(product) {
+        let { searchResults } = await invokeLambda({ functionName: 'getSearchResults', payload: { product } });
+        // if (hrefs) {
+        //   console.log(hrefs.length);
+        //   console.log(product);
+        //   for (const href of hrefs) {
+        //     if (!visitedURLs.includes(href)) {
+        //       visitedURLs.push(href);
+        //       insertQueue(href);
+        //     }
+        //   }
+        // } else {
+        //   // console.log('no hrefs');
+        // }
+        // return url;
+    }
+
+    const wait = () => {
+        return new Promise((resolve, reject) => {
+            crawlQueue.start((err) => {
+                if (err) console.log(err);
+                else {
+                    console.log(`visitedURLs: ${visitedURLs.length}`);
+                    console.log(`products: ${products.length}`);
+                    resolve({
+                        visitedURLs,
+                        products,
+                    });
+                }
+            });
+        });
+    };
+
+    const visitedURLs = [];
+    const products = [];
+
+    crawlQueue.push(async (cb) => {
+        visitedURLs.push(url);
+        const result = await scanUrl(url);
+        cb(null, result);
     });
-  }
 
-  const visitedURLs = [];
-  const products = [];
-
-  crawlQueue.push(async (cb) => {
-    visitedURLs.push(url);
-    const result = await scanUrl(url);
-    cb(null, result);
-  });
-
-  return await wait();
+    return await wait();
 };
--- research/index.js
+++ research/index.js
@@ -1,61 +1,57 @@
-const WAE = require("jsonld-parser").default;
-const puppeteer = require("puppeteer");
-const Url = require("url-parse");
-// const axios = require('axios');
-const jsdom = require("jsdom");
+const WAE = require('jsonld-parser').default;
+const puppeteer = require('puppeteer');
+const Url = require('url-parse');
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 process.setMaxListeners(0);
 
 const originalConsoleError = console.error;
 console.error = (msg) => {
-  if(msg.startsWith(msg, 'Error: Could not parse CSS stylesheet')) return;
-  originalConsoleError(msg);
+    if (msg.startsWith(msg, 'Error: Could not parse CSS stylesheet')) return;
+    originalConsoleError(msg);
 };
 
-const { getAmazonProductByUrl } = require("./vendors/amazon");
+const { getAmazonProductByUrl } = require('./vendors/amazon');
 
 function ConvertKeysToLowerCase(obj) {
-  var key, keys = Object.keys(obj);
-  var n = keys.length;
-  var newobj={}
-  while (n--) {
-    key = keys[n];
-    newobj[key.toLowerCase()] = obj[key];
-  }
-  return newobj;
+    var key,
+        keys = Object.keys(obj);
+    var n = keys.length;
+    var newobj = {};
+    while (n--) {
+        key = keys[n];
+        newobj[key.toLowerCase()] = obj[key];
+    }
+    return newobj;
 }
 
 const urls = [
-  // "http://mbrouka.ma/?product=nordic-modern-abstract-geometric-fox-crafts-desktop-ornaments-creative-for-office-home-decorations",
-  // "https://bearandpartner.com/products/nordic-modern-abstract-geometric-fox-crafts-desktop-ornaments-creative-for-office-home-decorations-animal-resin-crafts",
-  // "https://www.cicig.co/product/if7xter",
-  // "https://www.zipy.co.il/p/%D7%90%D7%9C%D7%99%D7%90%D7%A7%D7%A1%D7%A4%D7%A8%D7%A1/nordic-modern-abstract-geometric-fox-crafts-desktop-ornaments-creative-for-office-home-decorations-animal-resin-crafts/4000456883534/",
-  // "https://articulo.mercadolibre.com.mx/MLM-855119757-regalos-coleccionables-del-arbol-de-navidad-del-hogar-de-s-_JM",
-  // "https://www.aliexpress.com/item/33004270684.html?spm=a2g0o.productlist.0.0.5fa86d94xaiJIU&algo_pvid=6d6400bf-6de8-42ef-ad46-ec9076ad7c61&algo_expid=6d6400bf-6de8-42ef-ad46-ec9076ad7c61-0&btsid=0b0a555416105565137134031e3497&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
-  // "https://www.ebay.com/itm/Resin-Fox-Statue-Nordic-Modern-Abstract-Figurine-Office-Home-Animal-Ornament/373405583884?_trkparms=aid%3D1110006%26algo%3DHOMESPLICE.SIM%26ao%3D1%26asc%3D20200818143132%26meid%3D2289976ddc9748c699e2bce39fa445ba%26pid%3D101198%26rk%3D1%26rkt%3D12%26mehot%3Dnone%26sd%3D254660575517%26itm%3D373405583884%26pmt%3D1%26noa%3D0%26pg%3D2047675%26algv%3DSimplAMLv5PairwiseWebWithDarwoV3BBEV2b%26brand%3DUnbranded&_trksid=p2047675.c101198.m1985",
-  // "https://www.kaekoo.com/product-page/houndstooth",
-  // "http://www.snapit.co.il/product/%d7%9e%d7%93%d7%a4%d7%a1%d7%aa-%e2%80%8f%d7%94%d7%96%d7%a8%d7%a7%d7%aa-%d7%93%d7%99%d7%95-epson-ecotank-l3156-%d7%90%d7%a4%d7%a1%d7%95%d7%9f/",
-  // "http://www.matara-g.com/items/2077521-HL-L2310D-Brother",
-  // "https://www.toner-supply.co.il/product/%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%E2%80%8F%D7%9C%D7%99%D7%99%D7%96%D7%A8-hp-laser-107w-4zb78a",  
-  // "http://www.doricoh.co.il/items/912333-%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-BROTHER-HL-L9200DWT",
-  // "https://www.dr-dyo.co.il/items/616536-%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-%D7%A6%D7%91%D7%A2-%D7%91%D7%A8%D7%93%D7%A8-Brother-HL-L9200DWT",
-  // "https://www.dioplus.co.il/items/1283171-%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-%D7%91%D7%A8%D7%93%D7%A8-%D7%A6%D7%91%D7%A2%D7%95%D7%A0%D7%99%D7%AA-Brother-HL-L9200DWT",
-  // "https://www.jetcopy.co.il/%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-brother-hl-l9200cdwt",
-  // "https://www.shooks.co.il/p163418",
-  // "https://www.izzi.co.il/product/mdfst-iiizr-brother-hl-l9200cdwt-brdr.html",
-  // "https://www.zork.co.il/product-details/10899/%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-Brother-HL-L9200DWT-113212",
-  // "https://www.shopex.co.il/products/product_893347?locale=he",
-  // "https://www.amazon.ca/dp/B08HWZMSJY",
-  "https://www.amazon.es/dp/B08739TY2T",
-  // "https://www.amazon.de/dp/B07ZCS93VT",
+    // "http://mbrouka.ma/?product=nordic-modern-abstract-geometric-fox-crafts-desktop-ornaments-creative-for-office-home-decorations",
+    // "https://bearandpartner.com/products/nordic-modern-abstract-geometric-fox-crafts-desktop-ornaments-creative-for-office-home-decorations-animal-resin-crafts",
+    // "https://www.cicig.co/product/if7xter",
+    // "https://www.zipy.co.il/p/%D7%90%D7%9C%D7%99%D7%90%D7%A7%D7%A1%D7%A4%D7%A8%D7%A1/nordic-modern-abstract-geometric-fox-crafts-desktop-ornaments-creative-for-office-home-decorations-animal-resin-crafts/4000456883534/",
+    // "https://articulo.mercadolibre.com.mx/MLM-855119757-regalos-coleccionables-del-arbol-de-navidad-del-hogar-de-s-_JM",
+    // "https://www.aliexpress.com/item/33004270684.html?spm=a2g0o.productlist.0.0.5fa86d94xaiJIU&algo_pvid=6d6400bf-6de8-42ef-ad46-ec9076ad7c61&algo_expid=6d6400bf-6de8-42ef-ad46-ec9076ad7c61-0&btsid=0b0a555416105565137134031e3497&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_",
+    // "https://www.ebay.com/itm/Resin-Fox-Statue-Nordic-Modern-Abstract-Figurine-Office-Home-Animal-Ornament/373405583884?_trkparms=aid%3D1110006%26algo%3DHOMESPLICE.SIM%26ao%3D1%26asc%3D20200818143132%26meid%3D2289976ddc9748c699e2bce39fa445ba%26pid%3D101198%26rk%3D1%26rkt%3D12%26mehot%3Dnone%26sd%3D254660575517%26itm%3D373405583884%26pmt%3D1%26noa%3D0%26pg%3D2047675%26algv%3DSimplAMLv5PairwiseWebWithDarwoV3BBEV2b%26brand%3DUnbranded&_trksid=p2047675.c101198.m1985",
+    // "https://www.kaekoo.com/product-page/houndstooth",
+    // "http://www.snapit.co.il/product/%d7%9e%d7%93%d7%a4%d7%a1%d7%aa-%e2%80%8f%d7%94%d7%96%d7%a8%d7%a7%d7%aa-%d7%93%d7%99%d7%95-epson-ecotank-l3156-%d7%90%d7%a4%d7%a1%d7%95%d7%9f/",
+    // "http://www.matara-g.com/items/2077521-HL-L2310D-Brother",
+    // "https://www.toner-supply.co.il/product/%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%E2%80%8F%D7%9C%D7%99%D7%99%D7%96%D7%A8-hp-laser-107w-4zb78a",
+    // "http://www.doricoh.co.il/items/912333-%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-BROTHER-HL-L9200DWT",
+    // "https://www.dr-dyo.co.il/items/616536-%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-%D7%A6%D7%91%D7%A2-%D7%91%D7%A8%D7%93%D7%A8-Brother-HL-L9200DWT",
+    // "https://www.dioplus.co.il/items/1283171-%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-%D7%91%D7%A8%D7%93%D7%A8-%D7%A6%D7%91%D7%A2%D7%95%D7%A0%D7%99%D7%AA-Brother-HL-L9200DWT",
+    // "https://www.jetcopy.co.il/%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-brother-hl-l9200cdwt",
+    // "https://www.shooks.co.il/p163418",
+    // "https://www.izzi.co.il/product/mdfst-iiizr-brother-hl-l9200cdwt-brdr.html",
+    // "https://www.zork.co.il/product-details/10899/%D7%9E%D7%93%D7%A4%D7%A1%D7%AA-%D7%9C%D7%99%D7%99%D7%96%D7%A8-Brother-HL-L9200DWT-113212",
+    // "https://www.shopex.co.il/products/product_893347?locale=he",
+    // "https://www.amazon.ca/dp/B08HWZMSJY",
+    'https://www.amazon.es/dp/B08739TY2T',
+    // "https://www.amazon.de/dp/B07ZCS93VT",
 ];
 
-const exclude_services = [
-  "facebook.net",
-  "facebook.com",
-  "google-analytics.com",
-];
+const exclude_services = ['facebook.net', 'facebook.com', 'google-analytics.com'];
 
 // http://data-vocabulary.org
 // http://schema.org/product
@@ -63,92 +59,81 @@ const exclude_services = [
 // https://schema.org/Product
 
 const getProductSchema = (html) => {
-  html = html
-    .replace(/http:\/\/schema.org/gm, "https://schema.org")
-    .replace(/https:\/\/schema.org\/product/gm, "https://schema.org/Product")
-    .replace(/http:\/\/data-vocabulary.org\/Product/gm, "https://schema.org/Product");
-  const dom = new JSDOM(html);
-  const { window } = dom;
-  const { document } = window;
-  const ldScripts = document.querySelectorAll(
-    'script[type="application/ld+json"]'
-  );
-  let product;
-  if (ldScripts.length > 0) {
-    for (ldScript of ldScripts) {
-      let { textContent } = ldScript;
-      textContent = textContent.replace(/\s*[\r\n]/gm, "");
-      const jsonLd = JSON.parse(
-        ldScript.textContent.replace(/\s*[\r\n]/gm, "")
-      );
-      if (Array.isArray(jsonLd)) {
-        product = jsonLd.find(
-          (item) => item["@type"].indexOf("Product") !== -1
-        );
-      } else {
-        const type = jsonLd["@type"];
-        if (type) {
-          if (jsonLd["@type"].indexOf("Product") !== -1) {
-            product = jsonLd;
-          }
-        } else {
-          const graph = jsonLd["@graph"];
-          if (graph)
-            product = graph.find(
-              (item) => item["@type"].indexOf("Product") !== -1
-            );
+    html = html
+        .replace(/http:\/\/schema.org/gm, 'https://schema.org')
+        .replace(/https:\/\/schema.org\/product/gm, 'https://schema.org/Product')
+        .replace(/http:\/\/data-vocabulary.org\/Product/gm, 'https://schema.org/Product');
+    const dom = new JSDOM(html);
+    const { window } = dom;
+    const { document } = window;
+    const ldScripts = document.querySelectorAll('script[type="application/ld+json"]');
+    let product;
+    if (ldScripts.length > 0) {
+        for (ldScript of ldScripts) {
+            let { textContent } = ldScript;
+            textContent = textContent.replace(/\s*[\r\n]/gm, '');
+            const jsonLd = JSON.parse(ldScript.textContent.replace(/\s*[\r\n]/gm, ''));
+            if (Array.isArray(jsonLd)) {
+                product = jsonLd.find((item) => item['@type'].indexOf('Product') !== -1);
+            } else {
+                const type = jsonLd['@type'];
+                if (type) {
+                    if (jsonLd['@type'].indexOf('Product') !== -1) {
+                        product = jsonLd;
+                    }
+                } else {
+                    const graph = jsonLd['@graph'];
+                    if (graph) product = graph.find((item) => item['@type'].indexOf('Product') !== -1);
+                }
+            }
         }
-      }
+    } else {
+        const ldHTML = document.querySelector('[itemtype="https://schema.org/Product"]').outerHTML;
+        const parsed = WAE().parse(ldHTML.replace(/\s*[\r\n]/gm, ''));
+        const { microdata } = parsed;
+        const { Product } = microdata;
+        product = Product[0];
     }
-  } else {
-    const ldHTML = document.querySelector(
-      '[itemtype="https://schema.org/Product"]'
-    ).outerHTML;
-    const parsed = WAE().parse(ldHTML.replace(/\s*[\r\n]/gm, ""));
-    const { microdata } = parsed;
-    const { Product } = microdata;
-    product = Product[0];
-  }
 
-  product = ConvertKeysToLowerCase(product);
-  
-  // creates a bug. turns image to array of letters
-  console.log(product);
-  return product;
+    product = ConvertKeysToLowerCase(product);
+
+    // creates a bug. turns image to array of letters
+    console.log(product);
+    return product;
 };
 
 const getProduct = (url) =>
-  new Promise(async (resolve) => {
-    let product;
-    url = new Url(url);
-    const { host, href } = url;
-    const hostArr = host.split(".");
-    if (hostArr.includes("amazon")) {
-      product = await getAmazonProductByUrl(url);
-    } else {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.setRequestInterception(true);
-      // page.on('error', err => console.log('error happen at the page: ', err));
-      // page.on('pageerror', pageerr => console.log('pageerror occurred: ', pageerr));
-      page.on("request", (request) => {
-        if (["image", "stylesheet", "font"].indexOf(request.resourceType()) !== -1) request.abort();
-        else if (exclude_services.some(v => request._url.includes(v))) request.abort();
-        else request.continue();
-      });
-      await page.goto(href, { waitUntil: "networkidle0", timeout: 15000 });
-      const html = await page.content();
-      product = getProductSchema(html);
-      await browser.close();
-    }
-    resolve(product);
-  });
+    new Promise(async (resolve) => {
+        let product;
+        url = new Url(url);
+        const { host, href } = url;
+        const hostArr = host.split('.');
+        if (hostArr.includes('amazon')) {
+            product = await getAmazonProductByUrl(url);
+        } else {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.setRequestInterception(true);
+            // page.on('error', err => console.log('error happen at the page: ', err));
+            // page.on('pageerror', pageerr => console.log('pageerror occurred: ', pageerr));
+            page.on('request', (request) => {
+                if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) request.abort();
+                else if (exclude_services.some((v) => request._url.includes(v))) request.abort();
+                else request.continue();
+            });
+            await page.goto(href, { waitUntil: 'networkidle0', timeout: 15000 });
+            const html = await page.content();
+            product = getProductSchema(html);
+            await browser.close();
+        }
+        resolve(product);
+    });
 
 (async () => {
-  console.time('scan');
-  const promises = [];
-  for (url of urls) promises.push(getProduct(url));
-  const results = await Promise.all(promises);
-  console.log(JSON.stringify(results,null,2));
-  console.timeEnd('scan');
+    console.time('scan');
+    const promises = [];
+    for (url of urls) promises.push(getProduct(url));
+    const results = await Promise.all(promises);
+    console.log(JSON.stringify(results, null, 2));
+    console.timeEnd('scan');
 })();
