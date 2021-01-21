d01aae5f943631280fce49620a4d37ad6c130f09
shopint2021 <shopint2021@gmail.com>
2021-01-21 06:11:56 +0200

    refactor scanWebsite

--- functions/scanWebsite/index.js
+++ functions/scanWebsite/index.js
@@ -1,37 +1,17 @@
 const queue = require('queue');
-const AWS = require('aws-sdk');
-
-const { STAGE, InvokePort } = process.env;
-const Lambda = new AWS.Lambda(
-    STAGE !== 'prod'
-        ? {
-              endpoint: `http://host.docker.internal:${InvokePort}`,
-              sslEnabled: false,
-          }
-        : undefined
-);
-
-async function invokeLambda({ functionName, payload }) {
-    const req = {
-        FunctionName: functionName,
-        Payload: JSON.stringify(payload),
-        InvocationType: 'RequestResponse',
-    };
-    const response = await Lambda.invoke(req).promise();
-    const { Payload } = response;
-    return JSON.parse(Payload);
-}
+const { invokeLambda } = require('./lambda');
 
 exports.handler = async (event) => {
     const { url } = event;
 
-    const crawlQueue = queue({ results: [] });
-    const searchQueue = queue({ results: [] });
-    const insertQueue = (queue, href) => {
+    const visitedURLs = [];
+    const products = [];
+    const crawlQueue = queue();
+    const searchQueue = queue();
+
+    const insertQueue = (queue, call, href) => {
         queue.push(async (cb) => {
-            const result = await scanUrl(href);
-            // console.log(result);
-            cb(null, result);
+            cb(null, await call(href));
         });
     };
 
@@ -41,64 +21,36 @@ exports.handler = async (event) => {
             payload: { url, parseHrefs: true, parseSchema: true },
         });
         if (product) {
-            product.url = url;
-            products.push(product);
-            console.log(JSON.stringify(product));
+            products.push({ url, ...product });
         }
-        if (hrefs) {
-            console.log(`hrefs: ${hrefs.length}`);
-            for (const href of hrefs) {
-                if (!visitedURLs.includes(href)) {
-                    visitedURLs.push(href);
-                    insertQueue(crawlQueue, href);
-                    insertQueue(searchQueue, href);
-                }
+
+        (hrefs || []).forEach((href) => {
+            if (!visitedURLs.includes(href)) {
+                visitedURLs.push(href);
+                insertQueue(crawlQueue, scanUrl, href);
+                insertQueue(searchQueue, searchProduct, href);
             }
-        }
+        });
         return url;
     };
 
     async function searchProduct(product) {
-        let { searchResults } = await invokeLambda({ functionName: 'getSearchResults', payload: { product } });
-        // if (hrefs) {
-        //   console.log(hrefs.length);
-        //   console.log(product);
-        //   for (const href of hrefs) {
-        //     if (!visitedURLs.includes(href)) {
-        //       visitedURLs.push(href);
-        //       insertQueue(href);
-        //     }
-        //   }
-        // } else {
-        //   // console.log('no hrefs');
-        // }
-        // return url;
+        // let { searchResults } = await invokeLambda({ functionName: 'getSearchResults', payload: { product } });
+        // console.log(searchResults);
     }
 
-    const executeCrawlQueue = () => {
-        return new Promise((resolve, reject) => {
-            crawlQueue.start((err) => {
-                if (err) console.log(err);
-                else {
-                    console.log(`visitedURLs: ${visitedURLs.length}`);
-                    console.log(`products: ${products.length}`);
-                    resolve({
-                        visitedURLs,
-                        products,
-                    });
-                }
-            });
+    const executeQueue = (queue) => {
+        return new Promise((res, rej) => {
+            queue.start((err) => (err ? rej(err) : res()));
         });
     };
 
-    const visitedURLs = [];
-    const products = [];
-
-    crawlQueue.push(async (cb) => {
-        visitedURLs.push(url);
-        const result = await scanUrl(url);
-        cb(null, result);
-    });
-
-    return await executeCrawlQueue();
+    await scanUrl(url);
+    await Promise.all([executeQueue(crawlQueue), executeQueue(searchQueue)]);
+    console.log(`visitedURLs: ${visitedURLs.length}`);
+    console.log(`products: ${products.length}`);
+    return {
+        products,
+        visitedURLs,
+    };
 };
