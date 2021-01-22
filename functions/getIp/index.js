a9f482bfca0a3e22ea1de1045b8611d5f2967c09
shopint2021 <shopint2021@gmail.com>
2021-01-22 20:55:22 +0200

    send getip

--- functions/getIp/index.js
+++ functions/getIp/index.js
@@ -1,1 +1,20 @@
-exports.handler = async (event) => {};
+const axios = require('axios');
+const url = 'https://www.myip.com/';
+
+exports.handler = async (event) => {
+    const getHtml = async (url) => {
+        return (
+            await axios.get(url, {
+                headers: {
+                    Referer: url,
+                    userAgent:
+                        'Mozilla/5.0 (X11; Linux x86_64; Storebot-Google/1.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+                },
+            })
+        ).data;
+    };
+
+    const html = await getHtml(url);
+
+    console.log(html.split('ip">')[1].split('<')[0]);
+};
