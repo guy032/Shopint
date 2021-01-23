74100bf554979042b17bf975abda410b19d99f26
shopint2021 <shopint2021@gmail.com>
2021-01-23 16:30:43 +0200

    test fetcher with 100 ip requests

--- functions/fetcher/index.js
+++ functions/fetcher/index.js
@@ -18,14 +18,22 @@ const camelCased = (myString) => {
         .join('');
 };
 
-exports.handler = async (event) => {
-    const randomRegion = Math.floor(Math.random() * regions.length);
+const getRandomIp = async () => {
+    const randomRegion = regions[Math.floor(Math.random() * regions.length)];
     const randomIndex = Math.floor(Math.random() * numPerRegion) + 1;
-    console.log(
-        await invokeRemoteLambda({
-            region: regionName,
-            functionName: `${baseName}Re${camelCased(randomRegion)}Id${randomIndex}`,
-            payload: {},
-        })
-    );
+    return await invokeRemoteLambda({
+        region: randomRegion,
+        functionName: `${baseName}Re${camelCased(randomRegion)}Id${randomIndex}`,
+        payload: {},
+    });
+};
+
+exports.handler = async (event) => {
+    const promises = [];
+    for (let i = 0; i < 100; i++) {
+        promises.push(getRandomIp());
+    }
+    const ips = await Promise.all(promises);
+    console.log(ips.length);
+    console.log([...new Set(ips)].length);
 };
