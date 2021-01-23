b77c04dd66d552f91350f655f5d824f3bb452e74
shopint2021 <shopint2021@gmail.com>
2021-01-23 12:28:08 +0200

    update generator

--- scrapers/generator.js
+++ scrapers/generator.js
@@ -17,10 +17,20 @@ const createFunction = async (regionName, currId) => {
     const dirName = `${baseName}Re${camelCased(regionName)}Id${currId}`;
     await fs.promises.mkdir(dirName);
     await util.promisify(fse.copy)(`./${baseName}Template/`, `./${dirName}/`);
-    await fs.readdir(`./${dirName}/`, (err, files) => {
-        console.log(files);
+    await fs.readdir(`./${dirName}/`, async (err, files) => {
+        for (const file of files) {
+            if (file !== 'node_modules') {
+                // assuming only files, TODO - handle cases of folders
+                const filePath = `./${dirName}/${file}`;
+                const content = await fs.promises.readFile(filePath, 'utf-8');
+                await fs.promises.writeFile(
+                    filePath,
+                    content.replace(/start-region-end/g, regionName).replace(/start-index-end/g, currId)
+                );
+            }
+        }
     });
-    await fs.promises.rmdir(`./${dirName}/`);
+    // await fs.promises.rmdir(`./${dirName}/`, { recursive: true });
 };
 
-createFunction(regions[0], 1);
+createFunction(regions[1], 3);
--- scrapers/getIpReEuCentral1Id1/index.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-const url = 'http://checkip.amazonaws.com';
-
-exports.handler = async (event) => {
-    return (await axios.get(url)).data;
-};
